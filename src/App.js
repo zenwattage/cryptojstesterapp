c02d5ffe947c7e3da5345bb12b7cf4d5068f586e
Scott Hansford <zenwattage@gmail.com>
2023-01-23 18:56:18 -0800

    breaking stuff

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 
 import './App.css';
-import DecryptionExample from './components/DecryptionExample';
-import EncryptionExample from './components/EncryptionExample';
+// import { BrowserRouter, Route, Switch, Link } from 'react-router-dom';
+import { DecryptionExample } from './components/DecryptionExample';
+import { EncryptionExample } from './components/EncryptionExample';
+
+
 
 function App() {
   return (
-    <>
+    <div className="App">
       <EncryptionExample />
       <DecryptionExample />
-    </>
+    </div>
   );
 }
 
